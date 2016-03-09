57823123a118d695a775ecec7ec0c512b2992606
BENSIMON CHARLES <3000120@ppti-14-303-10.ufr-info-p6.jussieu.fr>
2016-03-09 17:46:13 +0100

    Initial commit

---
+++ taltools.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 14:53:04 2016
+
+@author: 3000120
+"""
+
+import numpy as np
+from scipy.sparse import csr_matrix
+
+def string2trigram(s):
+    size = len(s)
+    nps = np.fromstring(s, dtype=np.uint8).astype(int) % 128
+    tab3G = np.zeros((size+2, 3), int)
+    
+    tab3G[:-2, 0] = nps * 128**2
+    tab3G[1:-1, 1] = nps * 128
+    tab3G[2:, 2] = nps
+    
+    tab3G = tab3G.sum(axis=1)
+    
+    return csr_matrix(np.bincount(tab3G, minlength=128**3)).astype(np.uint32)
